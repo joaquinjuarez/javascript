784cda14fc253b70ab2eaca6619ac810806dd78f
joaquinjuarez <149271360+joaquinjuarez@users.noreply.github.com>
2023-12-13 22:26:30 -0300

    segunda entrega

--- mian.js
+++ mian.js
@@ -1,42 +1,69 @@
 function startGame() {
-    console.log("Welcome to the RPG Adventure!");
-  
-    let playerName = prompt("Enter your character's name:");
-    console.log(`Hello, ${playerName}! Your journey begins.`);
-  
-    while (true) {
-      console.log("\n--- Encounter ---");
-      console.log("A wild monster appears!");
-  
-      let choice = prompt("What will you do? (attack/run)");
-  
-      if (choice.toLowerCase() === "attack") {
-        if (Math.random() < 0.5) {
-          console.log("You defeated the monster! Victory!");
-        } else {
-          console.log("Oh no! The monster defeated you. Game over.");
-          break;
-        }
-      } else if (choice.toLowerCase() === "run") {
-        if (Math.random() < 0.5) {
-          console.log("You successfully escaped!");
-        } else {
-          console.log("Oops, you couldn't escape! The monster caught you. Game over.");
-          break;
-        }
+  console.log("Welcome to the RPG Adventure!");
+
+  let player = {
+    name: prompt("Enter your character's name:"),
+    health: 100,
+  };
+
+  console.log(`Hello, ${player.name}! Your journey begins.`);
+
+  let monsters = [
+    { name: "Goblin", health: 30 },
+    { name: "Orc", health: 50 },
+    { name: "Dragon", health: 100 },
+  ];
+
+  function battleRound(attacker, defender) {
+    console.log(`\n--- ${attacker.name}'s Turn ---`);
+
+    if (Math.random() < 0.5) {
+      if (Math.random() < 0.1) {
+        console.log(`${defender.name} blocks ${attacker.name}'s attack! No damage.`);
       } else {
-        console.log("Invalid choice. Please enter 'attack' or 'run'.");
+        let damage = Math.floor(Math.random() * 11) + 10;
+
+        defender.health -= damage;
+
+        console.log(`${attacker.name} hits ${defender.name} for ${damage} damage!`);
+
+        if (defender.health <= 0) {
+          console.log(`You defeated the ${defender.name}! Victory!`);
+          return false; 
+        }
       }
-    }
-  
-    let playAgain = prompt("Do you want to play again? (yes/no)");
-  
-    if (playAgain.toLowerCase() === "yes") {
-      startGame(); 
     } else {
-      console.log("Thanks for playing! Goodbye.");
+      console.log(`${attacker.name}'s attack missed!`);
+    }
+
+    return true; 
+  }
+
+  while (player.health > 0) {
+    let randomMonster = monsters[Math.floor(Math.random() * monsters.length)];
+
+    console.log(`\n--- Encounter: ${randomMonster.name} ---`);
+    
+    let continueBattle = battleRound(player, randomMonster);
+
+    if (!continueBattle) {
+      break; 
+    }
+
+    continueBattle = battleRound(randomMonster, player);
+
+    if (!continueBattle) {
+      break; 
     }
   }
-  
-  startGame();
-  
+
+  let playAgain = prompt("Do you want to play again? (yes/no)");
+
+  if (playAgain.toLowerCase() === "yes") {
+    startGame(); 
+  } else {
+    console.log("Thanks for playing! Goodbye.");
+  }
+}
+
+startGame();
